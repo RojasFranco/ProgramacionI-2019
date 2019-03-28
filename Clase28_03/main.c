ef7a8e7741f789b851064b0ebed7d27983788ed2
s3baRr00 <sebastianrilo@gmail.com>
2019-03-28 20:46:05 -0300

    validacion int terminada

--- Clase28_03/main.c
+++ Clase28_03/main.c
@@ -2,21 +2,60 @@
 #include <stdlib.h>
 
 int getInt(char *mensaje, char *mensajeError, int minimo, int maximo, int reintentos, int *resultado);
-
+int isValidInt(int numero, int minimo, int maximo);
+// Validamos int
+//devuelve 1 si V o 0 f
+//si esto vuelve v, lo devuelvo(la var apuntada por resultado)
 int main()
 {
-    printf("Hello world!\n");
+    int numeroIngresado;
+    int errorDeIngreso;
+    errorDeIngreso = getInt("Ingrese el numero", "Error", 0 , 50, 2, &numeroIngresado);
+    if (errorDeIngreso)
+    {
+        printf("Se ha ingresado algo invalido");
+    }
+    else
+    {
+        printf("El numero ingresado es: %d", numeroIngresado);
+    }
     return 0;
 }
 
-int isValidInt(int numero) // Validamos int
-//devuelve 1 si V o 0 f
-//si esto vuelve v, lo devuelvo(la var apuntada por resultado)
+int isValidInt(int numero, int minimo, int maximo)
+{
+    if (numero<=maximo && numero>=minimo)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 int getInt(char *mensaje, char *mensajeError, int minimo, int maximo, int reintentos, int *resultado)
-
-    inf buffer; // guardo aca lo que ingresa user
-    int retorno=-1
-    if(mensaje != NULL) // lo mismo con todos los punteros
-    //reintentos >=0
-        printf("%s", mensaje);
+{
+    int buffer; // aca guardo lo que ingresa user
+    int i=0;
+    int retorno=-1;
+    if(mensaje != NULL && mensajeError != NULL && resultado!= NULL && reintentos>=0 && maximo>=minimo)
+    {
+        for (i=0; i<=reintentos; i+=1);
+        {
+            printf("%s", mensaje); // para imprimir el mensaje
+            scanf("%d", buffer);
+            if (isValidInt(buffer, minimo, maximo))
+            {
+                *resultado = buffer;
+                return retorno;
+                break; // para no seguir iterando
+            }
+            else
+            {
+                printf("%s", mensajeError)
+            }
+        }
+        retorno = 0; //para cambiar el retorno
+    }
+}
