8b0e3e2640fad02dc290a2be71eb779f301bf969
s3baRr00 <sebastianrilo@gmail.com>
2019-03-28 19:32:32 -0300

    Biblioteca

---
+++ Clase28_03/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getInt(char *mensaje, char *mensajeError, int minimo, int maximo, int reintentos, int *resultado);
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+int isValidInt(int numero) // Validamos int
+//devuelve 1 si V o 0 f
+//si esto vuelve v, lo devuelvo(la var apuntada por resultado)
+
+int getInt(char *mensaje, char *mensajeError, int minimo, int maximo, int reintentos, int *resultado)
+
+    inf buffer; // guardo aca lo que ingresa user
+    int retorno=-1
+    if(mensaje != NULL) // lo mismo con todos los punteros
+    //reintentos >=0
+        printf("%s", mensaje);
