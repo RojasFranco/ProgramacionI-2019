2f3c6c40d96116ddc327496f464b4794aa70f174
s3baRr00 <sebastianrilo@gmail.com>
2019-04-25 21:46:58 -0300

    clase 10

--- clase6_Validaciones/main.c
+++ clase6_Validaciones/main.c
@@ -4,7 +4,7 @@
 
 int utn_getString(char* mensaje, char* Error,int minimo, int maximo, int reintentos, char* resultado);
 int utn_isValidNumber(char *str);
-int utn_getNumber(char* mensaje, char* Error,int minimo, int maximo, int reintentos, int resultado);
+int utn_getNumber(char* mensaje, char* Error,int minimo, int maximo, int reintentos, int* resultado);
 
 int main()
 {
@@ -12,7 +12,7 @@ int main()
     return 0;
 }
 
-int utn_getNumber(char* mensaje, char* Error,int minimo, int maximo, int reintentos, int resultado)
+int utn_getNumber(char* mensaje, char* Error,int minimo, int maximo, int reintentos, int* resultado)
 {
     int retorno=-1;
     char buffer[18]; /// podria pasar 16 (la misma cantidad
